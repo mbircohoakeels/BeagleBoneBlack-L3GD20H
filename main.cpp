0d45dda2e1ad0b510c7c1cb86c1d16547528a2e1
Michael Brookes <contactmichaelbrookes@gmail.com>
2016-02-20 15:55:04 +0000

    test this module using main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Michael Brookes on 20/02/2016.
+//
+
+#include "BBBL3GD20H.h"
+
+int main(void) {
+    BBBL3GD20H L3GD20H;
+    L3GD20H.Start();
+    usleep(5000);
+
+    while (1) {
+        for (int i = 0; i <= (MAX_DATASTORE - 1); i++) {
+            cout << "Gyro X Reading " << i << " : " << L3GD20H.GyroXDataStoredValues[i] << endl;
+            cout << "Gyro Y Reading " << i << " : " << L3GD20H.GyroYDataStoredValues[i] << endl;
+            cout << "Gyro Z Reading " << i << " : " << L3GD20H.GyroZDataStoredValues[i] << endl;
+        }
+        L3GD20H.PrintValuesToScreen();
+        usleep(5000);
+    }
+}
